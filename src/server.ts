33917f19ef0efb21bc09ad8ec9f42cf84e4a4c8e
Rickson <rickson.simoes@hotmail.com>
2020-08-07 12:30:37 -0300

    :package: Express/ :hammer: Rota Post

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('hello thereeee')
+import express, {Request, Response} from 'express';
+
+const app = express();
+
+app.use(express.json())
+
+app.listen(3333);
+
+app.get('/users', (req: Request, res:Response) => {
+  return res.json({msg: 'hello world :D'});
+})
