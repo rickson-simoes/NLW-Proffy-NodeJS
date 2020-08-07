3936fbc6fcf4bac7ca6b531b1f468c2546434656
Rickson <rickson.simoes@hotmail.com>
2020-08-07 16:15:45 -0300

    :hammer: :wrench: Configurando conexao com o banco ~ Criando tabelas / :package: Knex ~ Sqlite3 / :pencil: Scripts no package.json

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,9 @@ const app = express();
 
 app.use(express.json())
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('Server on ✌')
+});
 
 app.get('/users', (req: Request, res:Response) => {
   return res.json({msg: 'hello world :D'});
